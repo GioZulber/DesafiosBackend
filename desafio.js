f4275a132b6292c8f586f6c48e44daedad1b9414
GioZulber <giovannizulberti14@gmail.com>
2022-02-23 14:13:16 -0300

    funcion constructora

--- desafio.js
+++ desafio.js
@@ -1,4 +1,4 @@
-class Usuario {
+/*class Usuario {
   constructor(nombre, apellido, libros, mascotas) {
     this.nombre = nombre;
     this.apellido = apellido;
@@ -48,3 +48,42 @@ usuario1.addBook({
 });
 usuario1.getMacotas();
 usuario1.getBooks();
+*/
+function User(nombre, apellido, libros, mascotas) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.libros = libros;
+  this.mascotas = mascotas;
+}
+
+User.prototype.fullName = function () {
+  console.log(`${this.nombre} ${this.apellido}`);
+};
+
+User.prototype.addPet = function (newPet) {
+  this.mascotas.push(newPet);
+  console.log(this.mascotas);
+};
+User.prototype.pets = function () {
+  console.log(this.mascotas.length);
+};
+User.prototype.addBook = function (newBook) {
+  this.libros.push(newBook);
+};
+User.prototype.getBooks = function () {
+  this.libros.map((libro) => {
+    console.log(libro.nombre);
+  });
+};
+
+let user1 = new User(
+  'Giovanni',
+  'Zulberti',
+  [
+    {
+      nombre: 'Los Juegos del Hambre',
+      autor: 'Suzanne Collins',
+    },
+  ],
+  ['perro', 'gato']
+);
